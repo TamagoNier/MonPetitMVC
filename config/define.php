02e0584d974d06cd639171b92d2865f9c19918bb
Egor GUTUTUI <egor.gututui@lyc-bonaparte.fr>
2023-10-18 16:46:17 +0200

    correction erreur define et creation app exception + erreur html

--- config/define.php
+++ config/define.php
@@ -2,7 +2,7 @@
 //Define path
 define('PATH_CSS', RACINE. "assets".DS."css.".DS);
 define('PATH_JS', RACINE. "assets".DS."js.".DS);
-define('PATH_VENDOR', RACINE. "assets".DS."vendor".DS);
-define('PATH_VIEW', RACINE. "assets".DS."template".DS);
+define('PATH_VENDOR', RACINE. "vendor".DS);
+define('PATH_VIEW', RACINE. "template".DS);
 //on : mode Developpement; off : mode Prod
 define('MODE_DEV', 'on');

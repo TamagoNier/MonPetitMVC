5b090c6b8d8a7a6cdd839dd41ceb76e636438639
egor.gututui <egor.gututui@CP-SIO-W11-PG12.bts.sio>
2023-11-14 17:57:17 +0100

    creation classe repository

---
+++ tools/Repository.php
@@ -0,0 +1,34 @@
+<?php
+declare (strict_types=1);
+use Tools\Connexion;
+
+namespace Tools;
+
+abstract class Repository {
+
+    private string $classeNameLong;
+    private string $classeNamespace;
+    private string $table;
+    private PDO $connexion;
+    
+    private function __construct(string $entity) {
+        $tablo = explode("\\", $entity);
+        $this->table = array_pop($tablo);
+        $this->classeNamespace = implode("\\", $tablo);
+        $this->classeNameLong = $entity;
+        $this->connexion= Connexion::getConnexion();
+    }
+    
+    public static function getRepository(string $entity): Repository{
+        $repositoryName = str_replace('Entity', 'Repository', $entity) . 'Repository';
+        $repository = new $repositoryName($entity);
+        return $repository;
+    }
+    
+    public function findAll() : array{
+        $sql = "select * from ".$this->table;
+        $lignes= $this->connexion->query($sql);
+        $lignes->setFetchMode(PDO::FETCH_CLASS, $this->classeNameLong, null);
+        return $lignes->fetchAll();
+    }
+}
